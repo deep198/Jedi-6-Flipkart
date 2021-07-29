8ac963def7370e2ddb841c7403df88a055e88419
Pravallika Boppana <38985461+pravallika-boppana@users.noreply.github.com>
2021-07-29 22:40:32 +0530

    Delete CRS-POS directory

--- CRS-POS/src/com/flipkart/exception/NotApprovedException.java
+++
@@ -1,9 +0,0 @@
-package com.flipkart.exception;
-
-public class NotApprovedException extends Exception {
-
-    public String getMessage() {
-        String msg="You won't be able to login until Admin approves your Registration..";
-        return msg;
-    }
-}
