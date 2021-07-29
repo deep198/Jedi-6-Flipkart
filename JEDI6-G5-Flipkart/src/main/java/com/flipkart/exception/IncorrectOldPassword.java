0f5c7b736470b1964df7902fd59fd8658679a787
Pravallika Boppana <38985461+pravallika-boppana@users.noreply.github.com>
2021-07-29 22:43:56 +0530

    Delete JEDI6-G5-Flipkart directory

--- JEDI6-G5-Flipkart/src/main/java/com/flipkart/exception/IncorrectOldPassword.java
+++
@@ -1,9 +0,0 @@
-package com.flipkart.exception;
-
-public class IncorrectOldPassword extends Exception {
-
-public String getMessage() {
-        String msg = "Incorrect Old Password!!";
-        return msg;
-        }
-}
